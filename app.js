9130f55762407b2326bd82b1d5995f30e555ca30
FaizalKhan-AT <faizalkhanfk148@gmail.com>
2021-07-02 19:21:54 -0700

    photo editor for youtube video completed

--- app.js
+++ app.js
@@ -1,4 +1,7 @@
 const inputs = document.querySelectorAll('input');
+const button = document.querySelector('.fileUpload-btn')
+const fileInput = document.getElementById('fileUpload')
+const image = document.getElementById('image-container')
 inputs.forEach(input => {
      input.addEventListener('input',valueChanged)
 })
@@ -8,5 +11,13 @@ function valueChanged(e) {
         `--${e.target.name}`,
         e.target.value
     )
-    console.log(e.target.value);
 }
+button.addEventListener('click', () => {
+    fileInput.click();
+})
+fileInput.addEventListener('change', (e) => {
+    const [file] = fileInput.files;
+    if (file) {
+        image.src = URL.createObjectURL(file);
+    }
+})
